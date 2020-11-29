f08324fdfced9a70afe6aeeac0c5fe9c698d8c83
Kyle Koder <silvertallon@gmail.com>
2020-11-29 00:57:29 -0800

    1.12.2.001

--- src/main/java/com/fantasticsource/tiamatgamesettings/TiamatGameSettings.java
+++ src/main/java/com/fantasticsource/tiamatgamesettings/TiamatGameSettings.java
@@ -10,12 +10,12 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 
-@Mod(modid = TiamatGameSettings.MODID, name = TiamatGameSettings.NAME, version = TiamatGameSettings.VERSION, dependencies = "required-after:fantasticlib@[1.12.2.041f,)", acceptableRemoteVersions = "*")
+@Mod(modid = TiamatGameSettings.MODID, name = TiamatGameSettings.NAME, version = TiamatGameSettings.VERSION, dependencies = "required-after:fantasticlib@[1.12.2.042,)", acceptableRemoteVersions = "*")
 public class TiamatGameSettings
 {
     public static final String MODID = "tiamatgamesettings";
     public static final String NAME = "Tiamat Game Settings";
-    public static final String VERSION = "1.12.2.000";
+    public static final String VERSION = "1.12.2.001";
 
     @Mod.EventHandler
     public static void preInit(FMLPreInitializationEvent event)
