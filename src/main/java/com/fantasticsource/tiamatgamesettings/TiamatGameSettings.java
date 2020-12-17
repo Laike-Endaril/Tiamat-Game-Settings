bfd4df2470b2aaf23aad2fa9a9b464d243380521
Kyle Koder <silvertallon@gmail.com>
2020-12-17 15:28:25 -0800

    1.12.2.002a

--- src/main/java/com/fantasticsource/tiamatgamesettings/TiamatGameSettings.java
+++ src/main/java/com/fantasticsource/tiamatgamesettings/TiamatGameSettings.java
@@ -15,7 +15,7 @@ public class TiamatGameSettings
 {
     public static final String MODID = "tiamatgamesettings";
     public static final String NAME = "Tiamat Game Settings";
-    public static final String VERSION = "1.12.2.002";
+    public static final String VERSION = "1.12.2.002a";
 
     @Mod.EventHandler
     public static void preInit(FMLPreInitializationEvent event)
